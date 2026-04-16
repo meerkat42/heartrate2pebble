725ac54c157b2f2fd717f56c8caefecd43dd69c3
Rezwana Meer <rezwanam@gmail.com>
2026-04-15 18:35:32 -0700

    creating dynamically named releases

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0-beta"
+        versionName = "1.0.2-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
